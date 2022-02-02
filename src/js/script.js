37389bf5ee0a50548a10ef8d2ae2e87061b52b41
Timur <tim.sayfullaev@gmail.com>
2022-02-02 16:53:56 +0500

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        prevArrow: '<button type="button" class="reviews__arrow reviews__arrow_left"><img src="icons/rounded_rectangle_2_copy_7_493.png" alt="rounded_rectangle_2_copy_7_493"></button>',
+        nextArrow: '<button type="button" class="reviews__arrow reviews__arrow_right"><img src="icons/rounded_rectangle_2_copy_2_492.png" alt="rounded_rectangle_2_copy_2_492"></button>',
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 3
+    });
+});
