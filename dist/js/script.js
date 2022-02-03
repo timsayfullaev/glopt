8dd3b81f695ce51d3e4d2d97ba23fbb99c11bafc
Timur <tim.sayfullaev@gmail.com>
2022-02-04 01:54:40 +0500

    glopt

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,3 +15,22 @@ $(document).ready(function(){
         ]
     });
 });
+
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    menuClose = document.querySelector('.menu__close'),
+    menuLink = document.querySelectorAll('.menu__link');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('menu_active');
+});
+
+menuClose.addEventListener('click', () => {
+    menu.classList.remove('menu_active');
+});
+
+menuLink.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('menu_active');
+    });
+});
